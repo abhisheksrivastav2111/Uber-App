cc0978b95d5ab31fbbe4db075d0ca863257f6ecb
Abhishek Shrivatav <abhisheksrivastav2111@gmail.com>
2025-04-02 00:07:05 +0530

    project Complete

--- Backend/server.js
+++ Backend/server.js
@@ -2,11 +2,11 @@ const http = require('http');
 
 
 const app = require('./app');
-const {initializeSocket} = require('./socket')
+const {initializeSocket} = require('./socket');
 const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 
-initializeSocket(server)
+initializeSocket(server);
 
 
 
